901771581f6dcc20400af0033a9c4d1160d79108
Tom Hao <rabbitom@gmail.com>
2021-10-08 15:39:32 +0800

    feat: show connected device

--- lib/containers/BLESensors.jsx
+++ lib/containers/BLESensors.jsx
@@ -2,11 +2,39 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class BLESensors extends React.PureComponent {
     render() {
-        return <div>BLESensor</div>;
+        const { connectedDevices } = this.props;
+        return (
+            <div>
+                {connectedDevices && connectedDevices.size > 0 ? connectedDevices.map(device => <div>{device.name ? device.name : '<Unknown>'}</div>) : 'no device connected'}
+            </div>
+        );
     }
 }
 
-export default connect()(BLESensors);
+function mapStateToProps(state) {
+    const { adapter } = state.app;
+
+    const { selectedAdapter } = adapter;
+
+    if (!selectedAdapter) {
+        return {};
+    }
+
+    return {
+        connectedDevices: selectedAdapter.connectedDevices,
+    };
+}
+
+export default connect(mapStateToProps)(BLESensors);
+
+BLESensors.propTypes = {
+    connectedDevices: PropTypes.object,
+};
+
+BLESensors.defaultProps = {
+    connectedDevices: null,
+};
