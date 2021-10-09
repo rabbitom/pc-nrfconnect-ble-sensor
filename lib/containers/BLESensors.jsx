7f5b7f2b78d19cfc88b71180a7c78b8a927be739
Tom Hao <rabbitom@gmail.com>
2021-10-09 15:19:04 +0800

    feat: breadcrumb nav

--- lib/containers/BLESensors.jsx
+++ lib/containers/BLESensors.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import BLESensorList from '../components/BLESensorList';
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
 const defaultDeviceDefinition = require('../../NordicThingy52.json');
 
@@ -20,11 +21,35 @@ function getSensorList(deviceDefinition) {
 }
 
 class BLESensors extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            view: 'devices'
+        };
+    }
+
     render() {
         const { connectedDevices } = this.props;
         const sensors = getSensorList(defaultDeviceDefinition);
+        const { view } = this.state;
+        const navs = [{
+            title: 'Devices'
+        }]
+        if(view !== 'devices') {
+            navs.push({
+                title: 'Some Device'
+            })
+            if(view !== 'device')
+                navs.push({
+                    title: 'Some Sensor'
+                })
+        }
+        navs[navs.length - 1].active = true;
         return (
             <div>
+                <Breadcrumb>
+                    {navs.map(({title,active}) => <Breadcrumb.Item active={active || false}>{title}</Breadcrumb.Item>)}
+                </Breadcrumb>
                 {connectedDevices && connectedDevices.size > 0 ? connectedDevices.map(device => <div>{device.name ? device.name : '<Unknown>'}</div>) : 'no device connected'}
                 <BLESensorList sensors={sensors}/>
             </div>
