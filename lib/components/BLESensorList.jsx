8180c1bf40866a65fe9c9b264e16f878fefe6a5e
Tom Hao <rabbitom@gmail.com>
2021-10-09 20:09:56 +0800

    feat: sensor list

--- lib/components/BLESensorList.jsx
+++ lib/components/BLESensorList.jsx
@@ -2,15 +2,27 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
 
 const BLESensorList = ({
     device,
     sensors,
+    onReadSensorValue,
     onSelectSensor
 }) => {
     return (
         <div className="ble-sensor-list">
-            {sensors.map(sensor => <div className="ble-sensor-item" onClick={e => onSelectSensor(sensor,e)}>{sensor.name}</div>)}
+            {sensors.map(sensor => 
+                <div className="ble-sensor-item">
+                    <div className="ble-sensor-name">{sensor.name}</div>
+                    <div className="ble-sensor-value">
+                        {sensor.value}
+                        {sensor.unit}
+                    </div>
+                    <Button onClick={e => onReadSensorValue(device,sensor,e)}>Refresh</Button>&nbsp;
+                    <Button onClick={e => onSelectSensor(sensor,e)} variant="secondary">Chart</Button>
+                </div>
+            )}
         </div>
     );
 }
@@ -18,6 +30,7 @@ const BLESensorList = ({
 BLESensorList.propTypes = {
     device: PropTypes.object.isRequired,
     sensors: PropTypes.object.isRequired,
+    onReadSensorValue: PropTypes.func,
     onSelectSensor: PropTypes.func
 }
 
